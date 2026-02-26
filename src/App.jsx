bb3c438d7c772d0485745237821e37c3dd4bd8a7
Harshada Patil <harshupatil0405@gmail.com>
2026-02-26 18:04:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,14 @@ import BlurBlob from './components/BlurBlob';
 
 const App = () => {
   return (
-    <div className="bg-[#050414] min-h-screen flex flex-col">
+    <div className="bg-[#050414] min-h-screen flex flex-col relative overflow-x-hidden">
 
-       <BlurBlob position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} />
+      <BlurBlob 
+        position={{ top: '35%', left: '20%' }} 
+        size={{ width: '30%', height: '40%' }} 
+      />
 
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+      <div className="absolute inset-0 overflow-hidden bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
       
       <div className="relative pt-20 flex-grow">
         <Navbar />
@@ -26,6 +29,7 @@ const App = () => {
         <Education />
         <Contact />
       </div>
+
       <Footer/>
     </div>
   );
